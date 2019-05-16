a5012a3129a45f8d0406e267f96e13c1c42f903e
Azer90 <2168177860@qq.com>
2019-05-16 22:57:43 +0800

    update

--- app/Http/Controllers/WeChatController.php
+++ app/Http/Controllers/WeChatController.php
@@ -5,16 +5,25 @@ use App\Http\Controllers\Controller;
 use EasyWeChat;
 class WeChatController extends Controller
 {
+    private $app;
+    public function __construct()
+    {
+        $this->app = EasyWeChat::officialAccount();
+    }
+
     public function serve(){
 
-        $app = EasyWeChat::officialAccount();
-        $app->server->push(function ($message) {
-            return "您好！欢迎使用 EasyWeChat!";
+
+        $this->app->server->push(function ($message) {
+            return "您好！欢迎关注";
         });
-        $response = $app->server->serve();
+        $list = $this->app->menu->list();
+        $response = $this->app->server->serve();
 
 // 将响应输出
         return $response;
     }
+
+
 
 }
