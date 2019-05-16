2f267740f61691acedec8cf7eaa59d403f4d0f31
胡波 <991366809@qq.com>
2019-05-17 00:08:40 +0800

    登陆

--- app/Helper/functions.php
+++ app/Helper/functions.php
@@ -21,3 +21,36 @@ function Api_error($message = '', $data = array()) {
 
     return response()->json($result);
 }
+
+/**
+ * curl提交请求
+ * @param $url 提交地址
+ * @param array $data
+ * @param bool $https
+ * @return mixed
+ */
+function curl_post($url,$data=array(),$https=true)
+{
+    $curl = curl_init();                                    //创建一个新CURL资源 返回一个CURL句柄，出错返回 FALSE。
+    curl_setopt($curl, CURLOPT_REFERER,$_SERVER['HTTP_HOST']);           //构造来源
+    curl_setopt($curl, CURLOPT_USERAGENT,$_SERVER['HTTP_USER_AGENT']);     //模拟用户使用的浏览器
+    curl_setopt($curl, CURLOPT_CONNECTTIMEOUT,300);                    //在发起连接前等待的时间，如果设置为0，则无限等待。
+    curl_setopt($curl, CURLOPT_TIMEOUT, 300);                     //设置CURL允许执行的最长秒数
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER,true);               //获取的信息以文件流的形式返回，而不是直接输出。
+    if ($https) {                                                       //设置为https请求，不验证证书和hosts
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+    }
+    $header[] = 'ContentType:application/json;charset=UTF-8';
+    curl_setopt($curl, CURLOPT_HTTPHEADER, $header);               //用来设置HTTP头字段的数组
+    curl_setopt($curl, CURLOPT_URL, $url);                          //设置请求地址
+    curl_setopt($curl, CURLOPT_POST,true);                       //发送POST请求
+    curl_setopt($curl, CURLOPT_POSTFIELDS, $data);                  //发送的POST数据
+    curl_setopt($curl, CURLINFO_HEADER_OUT, TRUE);                //启用时追踪句柄的请求字符串
+    $result = curl_exec($curl);                               //执行CURL
+    if(curl_errno($curl)){                                  //检查是否有错误发生
+        echo 'Curl error: ' . curl_error($curl);                  //返回最后一次的错误号
+    }
+    curl_close($curl);                                     //关闭CURL 并且释放系统资源
+    return $result;
+}
---
+++ app/Http/Controllers/Api/LoginController.php
@@ -0,0 +1,37 @@
+<?php
+
+
+namespace App\Http\Controllers\Api;
+
+
+use App\Housings;
+use App\Http\Controllers\Controller;
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class LoginController extends Controller
+{
+
+    /**
+     * @return false|string
+     * 主页热门房源
+     */
+    public function login(Request $request)
+    {
+        $code = $request->input("code");
+        if(empty($code)){
+            return Api_error("缺少参数");
+        }
+        $url="https://api.weixin.qq.com/sns/jscode2session?appid=wx0d754fd3f7b4a131&secret=bb4e7d915336be487208b13b019dfce2&js_code=$code&grant_type=authorization_code";
+        $res = curl_post($url);
+        $wx_info = json_decode($res,true);
+        $userInfo = User::where("openid",$wx_info["openid"])->select("name,mobile,avatar,wchat_name,type,sex")->first();
+        if(!empty($userInfo)){
+            return Api_Success("登陆成功",$userInfo);
+        }else{
+            return Api_error("登陆失败");
+        }
+
+    }
+}
