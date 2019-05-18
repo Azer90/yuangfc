f29571a35c8b4985f95ddca1cea90453593b21e5
胡波 <991366809@qq.com>
2019-05-18 14:42:09 +0800

    注册

--- app/Http/Controllers/Api/LoginController.php
+++ app/Http/Controllers/Api/LoginController.php
@@ -64,14 +64,30 @@ class LoginController extends Controller
             if(empty($data["openid"])||empty($data["userInfo"])){
                 return Api_error("缺少参数");
             }
-            $insert = [
-                "open_id" => $data["openid"],
-                "wchat_name" => $data["userInfo"]["nickName"],
-                "avatar" => $data["userInfo"]["avatarUrl"],
-                "sex" => $data["userInfo"]["gender"],
-                "created_at" => date("Y-m-d H:i:s")
-            ];
-           $res = User::insert($insert);
+            $res=0;
+            $info = User::where("open_id",$data["openid"])->first();//查询当前用户是否存在
+            if(empty($info)){
+                $insert = [
+                    "open_id" => $data["openid"],
+                    "wchat_name" => $data["userInfo"]["nickName"],
+                    "avatar" => $data["userInfo"]["avatarUrl"],
+                    "sex" => $data["userInfo"]["gender"],
+                    "created_at" => date("Y-m-d H:i:s")
+                ];
+                $res = User::insert($insert);//添加新用户
+            }else{
+                if($data["userInfo"]["nickName"]!=$info["wchat_name"]||$data["userInfo"]["avatarUrl"]!=$info["avatar"]|| $data["userInfo"]["gender"]!=$info["sex"]){
+                    $insert = [
+                        "open_id" => $data["openid"],
+                        "wchat_name" => $data["userInfo"]["nickName"],
+                        "avatar" => $data["userInfo"]["avatarUrl"],
+                        "sex" => $data["userInfo"]["gender"],
+                        "update_at" => date("Y-m-d H:i:s")
+                    ];
+                    //信息变化，修改用户信息
+                    $res = User::where("open_id",$data["openid"])->update($insert);
+                }
+            }
            if($res > 0){
                return Api_success("注册成功",$res);
             }else{
