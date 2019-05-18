cd5336130e9fb6042e59c60774d77153cb3fde76
胡波 <991366809@qq.com>
2019-05-18 15:23:58 +0800

    是

--- app/Http/Controllers/Api/LoginController.php
+++ app/Http/Controllers/Api/LoginController.php
@@ -60,18 +60,27 @@ class LoginController extends Controller
     public function register(Request $request)
     {
         $data = $request->input();
+        $data["userInfo"] = json_decode($data["userInfo"],true);
+
         if($request->isMethod("post")){
+
+//            "userInfo" =>  "{"nickName":"路途","gender":1,"language":"zh_CN","city":"Nanchong","province":"Sichuan","country":"China","avatarUrl":"https://wx.qlogo.cn/mmopen/vi_32/guzbZrvrSA9OXQy5YGcFwUgJ90WdrOL9wiaVqVbnyjrqXajWuxINHl0HaSzB0ibFxojWxnnkcBcrLXccf8vQyHTw/132"}
+//  "openid" => "oNGBH426aQ4_4xNHRqVn_jNWPx90"
+
             if(empty($data["openid"])||empty($data["userInfo"])){
                 return Api_error("缺少参数");
             }
+
             $res=0;
             $info = User::where("open_id",$data["openid"])->first();//查询当前用户是否存在
+
             if(empty($info)){
                 $insert = [
                     "open_id" => $data["openid"],
                     "wchat_name" => $data["userInfo"]["nickName"],
                     "avatar" => $data["userInfo"]["avatarUrl"],
                     "sex" => $data["userInfo"]["gender"],
+                    "email" => "",
                     "created_at" => date("Y-m-d H:i:s")
                 ];
                 $res = User::insert($insert);//添加新用户
@@ -82,7 +91,7 @@ class LoginController extends Controller
                         "wchat_name" => $data["userInfo"]["nickName"],
                         "avatar" => $data["userInfo"]["avatarUrl"],
                         "sex" => $data["userInfo"]["gender"],
-                        "update_at" => date("Y-m-d H:i:s")
+                        "updated_at" => date("Y-m-d H:i:s")
                     ];
                     //信息变化，修改用户信息
                     $res = User::where("open_id",$data["openid"])->update($insert);
@@ -91,7 +100,7 @@ class LoginController extends Controller
            if($res > 0){
                return Api_success("注册成功",$res);
             }else{
-               return Api_error("登陆失败",$res);
+               return Api_error("注册失败",$res);
            }
         }
     }
--- routes/api.php
+++ routes/api.php
@@ -34,7 +34,7 @@ $attributes = [
 //    'domain'     => 'api.yuang.com',
 ];
 Route::group($attributes,function (Router $router) {
-    $router->get('api_login', "LoginController@login")->name('api_login');
-    $router->get('api_register', "LoginController@register")->name('api_register');
-    $router->get('api_getOpenId', "LoginController@getOpenId")->name('api_getOpenId');
+    $router->post('api_login', "LoginController@login")->name('api_login');
+    $router->post('api_register', "LoginController@register")->name('api_register');
+    $router->post('api_getOpenId', "LoginController@getOpenId")->name('api_getOpenId');
 });
