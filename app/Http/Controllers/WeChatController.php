bc15ae71a3a7baaa00bdeff72943d94b9d4b0849
Azer90 <2168177860@qq.com>
2019-05-19 09:30:44 +0800

    update

--- app/Http/Controllers/WeChatController.php
+++ app/Http/Controllers/WeChatController.php
@@ -20,7 +20,7 @@ class WeChatController extends Controller
             $monolog->popHandler();
             Log::useFiles(storage_path('logs/wechat_ts.log'));
             Log::info($message);
-            if($message['MsgType']=='SCAN'){
+            if($message['Event']=='SCAN'&&$message['EventKey']=='validate_logon'){
                 return "登录成功";
             }
 
