db006169dc2d79f339c5a376b2ba828522e728e7
胡波 <991366809@qq.com>
2019-05-23 23:16:58 +0800

    啊

---
+++ app/Recommend.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019/5/23
+ * Time: 23:06
+ */
+
+namespace App;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class Recommend extends Model
+{
+    protected $table="recommend";
+}
