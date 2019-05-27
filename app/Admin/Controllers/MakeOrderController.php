b55d7fc18dbc6c18134e794c184a9d7ff8cbd0a7
Azer90 <2168177860@qq.com>
2019-05-27 23:40:36 +0800

    update

--- app/Admin/Controllers/MakeOrderController.php
+++ app/Admin/Controllers/MakeOrderController.php
@@ -2,6 +2,7 @@
 
 namespace App\Admin\Controllers;
 
+use App\Housings;
 use App\MakeOrder;
 use App\Http\Controllers\Controller;
 use Encore\Admin\Controllers\HasResourceActions;
@@ -9,7 +10,7 @@ use Encore\Admin\Form;
 use Encore\Admin\Grid;
 use Encore\Admin\Layout\Content;
 use Encore\Admin\Show;
-
+use Encore\Admin\Widgets\Table;
 class MakeOrderController extends Controller
 {
     use HasResourceActions;
@@ -23,8 +24,8 @@ class MakeOrderController extends Controller
     public function index(Content $content)
     {
         return $content
-            ->header('Index')
-            ->description('description')
+            ->header('预约管理')
+            ->description('列表')
             ->body($this->grid());
     }
 
@@ -80,23 +81,60 @@ class MakeOrderController extends Controller
     protected function grid()
     {
         $grid = new Grid(new MakeOrder);
-
         $grid->id('Id');
-        $grid->house_id('House id');
-        $grid->agent_id('Agent id');
-        $grid->make_id('Make id');
-        $grid->make_name('Make name');
-        $grid->make_mobile('Make mobile');
-        $grid->ID_card('ID card');
-        $grid->time('Time');
-        $grid->time_slot('Time slot');
-        $grid->remark('Remark');
-        $grid->is_divide('Is divide');
-        $grid->state('State');
-        $grid->add_schedule('Add schedule');
-        $grid->created_at('Created at');
-        $grid->updated_at('Updated at');
+        $grid->title('预约房源')->modal('房源信息', function ($model) {
+            $comments = Housings::where("id",$model->house_id)->take(1)->get()->map(function ($comment) {
+                $data=$comment->only(['id','title','address','type', 'rentsale','owner','phone']);
+                if($data['rentsale']==1){
+                    $data['rentsale']='出售';
+                }elseif($data['rentsale']==2){
+                    $data['rentsale']='出租';
+                }else{
+                    $data['rentsale']='未知';
+                }
+                if($data['type']==1){
+                    $data['type']='新房';
+                }elseif($data['type']==2){
+                    $data['type']='二手房';
+                }else{
+                    $data['type']='未知';
+                }
+                return $data;
+            });
 
+            return new Table(['ID','标题','地址', '房源类型', '租售类型', '业主', '联系方式'], $comments->toArray(),['table-hover']);
+        });;
+        $grid->name('经纪人姓名');
+        $grid->wchat_name('预约人微信名');
+        $grid->make_name('称呼');
+        $grid->make_mobile('电话');
+        $grid->time('预约日期');
+        $grid->time_slot('时间段')->using([0 => '全天',1 => '上午',2 => '下午',3 => '晚上']);
+        $grid->remark('备注');
+
+        $grid->is_divide('是否分成')->radio([
+            0 => '未分成',
+            1 => '已分成',
+        ]);
+        $grid->state('是否看房')->radio([
+            0 => '未看房',
+            1 => '已看房',
+            2 => '已取消',
+        ]);
+        $grid->created_at('创建时间');
+
+        $grid->disableExport();//禁用导出
+        $grid->disableCreateButton();
+        $grid->actions(function ($actions) {
+            $actions->disableEdit();
+            $actions->disableView();
+            $actions->disableDelete();
+        });
+        $grid->tools(function ($tools) {
+            $tools->batch(function ($batch) {
+                $batch->disableDelete();
+            });
+        });
         return $grid;
     }
 
@@ -147,8 +185,16 @@ class MakeOrderController extends Controller
         $form->date('time', 'Time')->default(date('Y-m-d'));
         $form->switch('time_slot', 'Time slot');
         $form->text('remark', 'Remark');
-        $form->switch('is_divide', 'Is divide');
-        $form->switch('state', 'State');
+
+        $form->radio('is_divide', 'Is divide')->options([
+            0 => '未分成',
+            1 => '已分成',
+        ]);
+        $form->radio('state', 'State')->options([
+            0 => '未看房',
+            1 => '已看房',
+            2 => '已取消',
+        ]);
         $form->switch('add_schedule', 'Add schedule');
 
         return $form;
--- app/MakeOrder.php
+++ app/MakeOrder.php
@@ -1,17 +1,10 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Administrator
- * Date: 2019/5/22
- * Time: 22:56
- */
-
 namespace App;
 
-
-
+use Illuminate\Support\Facades\Request;
 use Illuminate\Database\Eloquent\Model;
-
+use Encore\Admin\Facades\Admin;
+use Illuminate\Pagination\LengthAwarePaginator;
 class MakeOrder extends Model
 {
     protected $table = 'make_order';
@@ -19,5 +12,49 @@ class MakeOrder extends Model
     public function housings()
     {
         return $this->belongsTo(Housings::class,"house_id");
+    }
+
+
+    public function paginate()
+    {
+
+        $perPage = Request::get('per_page', 20);
+
+        $page = Request::get('page', 1);
+
+        $start = ($page-1)*$perPage;
+        if(Admin::user()->isAdministrator()){
+            // 运行sql获取数据数组
+            $result = self::from('make_order as mo')->where('mo.add_schedule',1)
+                ->join('housings as h','h.id','=','mo.house_id')
+                ->join('users as u','u.id', '=', 'mo.agent_id')
+                ->join('users as u1','u1.id', '=', 'mo.make_id')
+                ->skip($start)->take($perPage)->orderBy('mo.id', 'desc')->get(['mo.*','h.title','u.name','u1.wchat_name'])->toArray();
+            $total =self::where('add_schedule',1)->count();
+        }else{
+            // 运行sql获取数据数组
+            $district_id=Admin::user()->district_id;
+            $result = self::from('make_order as mo')->where(['mo.add_schedule'=>1,'h.district_id'=>$district_id])
+                ->join('housings as h','h.id','=','mo.house_id')
+                ->join('users as u','u.id', '=', 'mo.agent_id')
+                ->join('users as u1','u1.id', '=', 'mo.make_id')
+                ->skip($start)->take($perPage)->orderBy('mo.id', 'desc')->get(['mo.*','h.title','u.name','u1.wchat_name'])->toArray();
+            $total =self::where('add_schedule',1)->count();
+        }
+
+        //dd($result);
+        $result = static::hydrate($result);
+
+
+        $paginator = new LengthAwarePaginator($result, $total, $perPage);
+
+        $paginator->setPath(url()->current());
+
+        return $paginator;
+    }
+
+    public static function with($relations)
+    {
+        return new static;
     }
 }
